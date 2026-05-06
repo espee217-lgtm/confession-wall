dc84944b0caa0d8ff7c88d81a411fa0820c66e6c
espee217-lgtm <Espee217@gmail.com>
2026-05-06 22:09:54 +0530

    add special access email to client

--- client/src/utils/specialAccess.js
+++ client/src/utils/specialAccess.js
@@ -2,6 +2,7 @@ export const SPECIAL_ALLOWED_EMAILS = [
   "mcr9496@gmail.com",
   "theexperttunic464@gmail.com",
   "espee217@gmail.com",
+  "noiorexd@gmail.com",
 ];
 
 export function normalizeEmail(email) {
